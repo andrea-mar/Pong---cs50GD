c000204614b280a2cd77ebc4f1cf87d051e22ba1
andrea-mar <andreanistormar@gmail.com>
2021-08-10 17:12:02 +0100

    made classes for ball and paddles and separated into files

--- main.lua
+++ main.lua
@@ -6,10 +6,14 @@ VIRTUAL_HEIGHT = 243
 
 PADDLE_SPEED = 200
 
+Class = require 'class'
 push = require 'push'
 
+require 'Ball'
+require 'Paddle'
 
--- initiate the game
+
+-- initialize the game
 function love.load()
     math.randomseed(os.time())
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -17,15 +21,17 @@ function love.load()
     smallFont = love.graphics.newFont('font.ttf', 8)
     scoreFont = love.graphics.newFont('font.ttf', 32)
 
+    -- initialize score at the start of the game
     player1score = 0
     player2score = 0
 
-    player1Y = 30
-    player2Y = VIRTUAL_HEIGHT - 50
+    paddle1 = Paddle(5, 20, 5, 20)
+    paddle2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
+    ball = Ball(VIRTUAL_WIDTH/2-2, VIRTUAL_HEIGHT/2-2, 4, 4)
     
     gameState = 'start'
-
-    resetBall()
+    -- start with the ball in the center of the screen
+    ball:reset()
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false, 
@@ -37,25 +43,30 @@ end
 
 function love.update(dt)
     -- movement for left paddle
+    paddle1:update(dt)
     if love.keyboard.isDown('w') then
-        player1Y = math.max(0, player1Y - PADDLE_SPEED * dt)
+        paddle1.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('s') then
-        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+        paddle1.dy = PADDLE_SPEED
+    else
+        paddle1.dy = 0
     end
 
     -- movement for right paddle
+    paddle2:update(dt)
     if love.keyboard.isDown('up') then
-        player2Y = math.max(0, player2Y - PADDLE_SPEED * dt)
+        paddle2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then
-        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+        paddle2.dy = PADDLE_SPEED
+    else
+        paddle2.dy = 0
     end
 
+    -- movement for the ball (only in play state)
     if gameState == 'play' then
-        ballX = ballX + ballDX * dt
-        ballY = ballY + ballDY * dt
+        ball:update(dt)
     end
 end
-
 
 
 function love.keypressed(key)
@@ -66,28 +77,28 @@ function love.keypressed(key)
             gameState = 'play'
         elseif gameState == 'play' then
             gameState = 'start'
-            resetBall()
+            ball:reset()
         end
     end
 end
 
 
 function love.draw()
-
-
     push:apply('start')                 -- eveything written after this is done the 'push' way
 
     -- draw the background color
     love.graphics.clear(62/255, 53/255, 65/255, 225/255)
 
     -- draw ball
-    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
-    --draw left paddle
-    love.graphics.rectangle('fill', 5, player1Y, 5, 20)
-    --draw right paddle
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
+    ball:render()
 
-    -- set title font
+    --draw left paddle
+    paddle1:render()
+    
+    --draw right paddle
+    paddle2:render()
+
+    -- set title font 
     love.graphics.setFont(smallFont)
     if gameState == 'start' then
         love.graphics.printf(
@@ -104,6 +115,7 @@ function love.draw()
             VIRTUAL_WIDTH,              
             'center')                  
     end
+    -- set score font 
     love.graphics.setFont(scoreFont)
     love.graphics.print(player1score, VIRTUAL_WIDTH/2 - 50, VIRTUAL_HEIGHT/3)
     love.graphics.print(player2score, VIRTUAL_WIDTH/2 + 30, VIRTUAL_HEIGHT/3)
@@ -112,18 +124,3 @@ function love.draw()
 end
 
 
--- centers the ball on the screen and gives it a random  speed
-function resetBall()
-    ballX = VIRTUAL_WIDTH / 2 - 2
-    ballY = VIRTUAL_HEIGHT / 2 - 2
-    --[[ this code doesnt work but it is explaining what the below line does
-    ballDX = math.random(2)
-    if ballDX == 1 then
-        ballDX = -100
-    else
-        ballDX = 100
-    end
-    ]]
-    ballDX = math.random(2) == 1 and -100 or 100
-    ballDY = math.random(-50, 50)
-end
